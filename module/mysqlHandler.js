5e44bde04023f3ac943a211f62045b8838884f7e
이혜지 <ihyeji@ihyejiui-MacBook-Pro.local>
2016-05-10 16:50:56 +0900

    update db with Raspberry Pi

---
+++ module/mysqlHandler.js
@@ -0,0 +1,40 @@
+var mysql = require('mysql');
+//pool is simmilar with thread. thread pool
+var pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'localhost',
+  user            : 'root',
+  password        : 'qwer1234',
+  database        : 'jariyo'
+});
+
+//exports = {} like an object
+exports.updateSpaceState = function(data, callback)
+{
+	console.log(data);
+	//parameter check
+	if(data.spaceId == undefined || data.isEmpty == undefined)
+	{
+		callback("parameter error");
+		return;
+	}
+
+	var sql = 'UPDATE ParkingSpace SET `empty`=? where `space_id`=?'
+	var params = [];
+	params.push(data.isEmpty);
+	params.push(data.spaceId);
+
+	pool.getConnection(function(err, connection) {
+		// Use the connection 
+		connection.query(sql, params, function(err, rows) {
+			// And done with the connection. 
+			connection.release();
+			callback(err, rows);
+			// Don't use the connection here, it has been returned to the pool. 
+		});
+	});
+}
+
+//{node_id:10} data.node_id
+//test code
+exports.updateSpaceState({spaceId:10, isEmpty: 1}, function(err, res){console.log(err, res)})
