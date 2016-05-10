9024ff464988440fc4410ab93ce3b9e460a11beb
이혜지 <ihyeji@ihyejiui-MacBook-Pro.local>
2016-05-11 03:36:16 +0900

    communication with app implemented

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
+/**
+ * Created by hyeji on 2016. 5. 8..
+ */
+
+//0: parameter undefined
+
 var express = require('express');
 var router = express.Router();
 var mysqlHandler = require('../module/mysqlHandler');
-//var dijkstraHandler = require('dijkstraHandler');
+var navigationHandler = require('../public/javascripts/navigation');
 
 /*POST home page.*/
+//req.body={spaceID, isEmpty}
 router.post('/led_lamp', function(req, res, next){ //{spaceId: 10, isEmpty: 0}
 	if(req.body.spaceId == undefined || req.body.isEmpty == undefined)
 	{
@@ -18,10 +25,25 @@ router.post('/led_lamp', function(req, res, next){ //{spaceId: 10, isEmpty: 0}
 	})
 
 });
+//req.body={node_id}
+router.post('/navi', function(req, res, next){
+	if(req.body.node_id == undefined)
+	{
+		res.json({error: 0});
+		return;
+	}
+	
+	navigationHandler.findPath(req.body, function (error, result) {
+		if(error)
+		{
+			console.log(error);
+			return;
+		}
+		
+		res.json(result);
+		
+	})
 
-// router.post('/navi', function(req, res, next){
 
-
-
-//});
+});
 module.exports = router;
